af4f06fec6331a192104b5d9652b7c3fd07c56ea
VAELIA5 <VAELIA5@10.131.100.57>
2017-03-14 14:06:45 +0100

    1er arbre, viewers

---
+++ com.sii.rental.ui/src/com/sii/rental/ui/views/RentalAgencies.java
@@ -0,0 +1,24 @@
+package com.sii.rental.ui.views;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.widgets.Composite;
+
+import com.opcoach.training.rental.RentalAgency;
+
+public class RentalAgencies {
+	@PostConstruct
+	public void createPartControl(Composite parent, RentalAgency a) {
+		TreeViewer tv = new TreeViewer(parent);
+		tv.setContentProvider(new RentalProvider());
+		tv.setLabelProvider(new RentalProvider());
+		
+		Collection<RentalAgency> agencies = new ArrayList<>();
+		agencies.add(a);
+		tv.setInput(agencies);
+	}
+}
